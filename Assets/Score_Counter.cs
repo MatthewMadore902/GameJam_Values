9eb5f44cea4bacf60dae755438f50cd1e18796be
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-11-20 21:33:03 -0400

    Added UI and local leaderboard

--- Assets/Score_Counter.cs
+++ Assets/Score_Counter.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class Score_Counter : MonoBehaviour
 {
     public TextMeshProUGUI CountText;
-    private float count;
+    private float Count;
     private float startTime;
     // Start is called before the first frame update
 
@@ -16,8 +16,9 @@ public class Score_Counter : MonoBehaviour
     //count sets to 0 on start of the game and SetCountText updates it as it goes along
     void Start()
     {
+       // GUI.skin.box.fontSize = 10;
         SetCountText();
-        count = 0;
+        Count = 0;
         startTime = Time.time;
     }
 
@@ -27,20 +28,27 @@ public class Score_Counter : MonoBehaviour
         //change Input to what we decide the enemys will be named for when we kill them
         if (Input.GetKeyDown(KeyCode.C))
         {
-            count = count + 100000;
+            Count = Count + 10;
         }
         SetCountText();
     }
 
-    //this sets the score and displays it on screen with the canvas and a text 
-    void SetCountText()
+	void OnGUI()
+	{
+		
+	}
+
+	//this sets the score and displays it on screen with the canvas and a text 
+	// you can also change the color of the text through <color=blahblahblah>
+	void SetCountText()
     {
-        CountText.text = "Score: " + count.ToString();
+        CountText.text = "<color=#c0c0c0ff><size=25>Score:</size></color> " + Count.ToString("F0");
     }
 
 	void FixedUpdate()
 	{
+        //set the int to however fast you want the score to go up
         //count = System.Math
-        count = count + 1 * Time.deltaTime;
+        Count = Count + 100 * Time.deltaTime;
 	}
 }
